20f1b94476ba71e2e804165e13c5e9c9d2580e57
ABHIJEET SINHA <65067655+Abhijeetsinha471@users.noreply.github.com>
2022-06-08 20:49:38 -0700

    created components for list

---
+++ src/Components/List.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const List = () =>{
+    return (
+        <div>
+          <h1>Mobile OPerating System</h1>
+          <ul>
+              <li>Android</li>
+              <li>Blackberry</li>
+              <li>iPhone</li>
+              <li>Windows Phone</li>
+          </ul>
+          <h2>Mobile Manufactural</h2>
+          <ul>
+              <li>Samsung</li>
+              <li>HTC</li>
+              <li>Micromax</li>
+              <li>Apple</li>
+          </ul>
+        </div>
+    )
+}
+export {List};
